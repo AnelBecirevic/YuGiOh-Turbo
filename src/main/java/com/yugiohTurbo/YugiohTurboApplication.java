a168b60a50f98aa695b30bb11e0d9f09590434b4
Anel Becirevic <mfubyt@hotmail.com>
2026-08-02 18:12:38 +0200

    Initial Spring Boot project setup

---
+++ src/main/java/com/yugiohTurbo/YugiohTurboApplication.java
@@ -0,0 +1,13 @@
+package com.yugiohTurbo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YugiohTurboApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YugiohTurboApplication.class, args);
+	}
+
+}
